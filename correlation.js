29afe91f8836204267fd38f874906fcaf79d8d55
David Wilson <david@davidludwigwilson.me>
2015-06-20 18:21:50 +0100

    added correlation module

--- correlation.js
+++ correlation.js
@@ -1,5 +1,13 @@
 var jStat = require('jStat').jStat;
 
 
+module.exports = {
+ cor : function (one, two) {
+	var oneVec = jStat.seq(one);
+	var twoVec = jStat.seq(two);
+	return jStat.corrcoeff(oneVec, twoVec);
+}
 
-var cor = function 
+
+
+};
